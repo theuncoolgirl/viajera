9512f64d2ff62393a40efcc04ead064875494572
Erin Shields <elshields92@gmail.com>
2020-11-13 17:30:16 -0300

    refactor navbar to independent component

--- server/frontend/src/axiosApi.js
+++ server/frontend/src/axiosApi.js
@@ -17,9 +17,10 @@ axiosInstance.interceptors.response.use(
     response => response,
     error => {
         const originalRequest = error.config;
-
+        console.log("111")
         // Prevent infinite loops early
         if (error.response.status === 401 && originalRequest.url === baseURL + 'token/refresh/') {
+            console.log("222")
             window.location.href = '/login/';
             return Promise.reject(error);
         }
@@ -27,9 +28,11 @@ axiosInstance.interceptors.response.use(
         if (error.response.data.code === "token_not_valid" &&
             error.response.status === 401 &&
             error.response.statusText === "Unauthorized") {
+            console.log("333")
             const refreshToken = localStorage.getItem('refresh_token');
 
             if (refreshToken) {
+                console.log("444")
                 const tokenParts = JSON.parse(atob(refreshToken.split('.')[1]));
 
                 // exp date in token is expressed in seconds, while now() returns milliseconds:
@@ -37,10 +40,11 @@ axiosInstance.interceptors.response.use(
                 console.log("Hitting Here2!: ", tokenParts.exp);
 
                 if (tokenParts.exp > now) {
+                    console.log("555")
                     return axiosInstance
                         .post('/token/refresh/', { refresh: refreshToken })
                         .then((response) => {
-
+                            console.log("666")
                             localStorage.setItem('access_token', response.data.access);
                             localStorage.setItem('refresh_token', response.data.refresh);
 
@@ -50,13 +54,16 @@ axiosInstance.interceptors.response.use(
                             return axiosInstance(originalRequest);
                         })
                         .catch(err => {
+                            console.log("777")
                             console.log("Hitting here1!", err)
                         });
                 } else {
+                    console.log("888")
                     console.log("Refresh token is expired", tokenParts.exp, now);
                     window.location.href = '/login/';
                 }
             } else {
+                console.log("999")
                 console.log("Refresh token not available.")
                 window.location.href = '/login/';
             }
