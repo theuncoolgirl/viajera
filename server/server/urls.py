e25818c98ebe61c3c0362d7fc7a0426879644109
Erin Shields <elshields92@gmail.com>
2020-11-24 13:32:17 -0300

    added whitenoise and related files

--- server/server/urls.py
+++ server/server/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import FrontendAppView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('viajara.urls')),
-    path('', include('frontend.urls')),
-    re_path(r'^', FrontendAppView.as_view())
+    path('', include('frontend.urls'))
 ]
