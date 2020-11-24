b1fe1b1d2633ad717e2b88ad11b1bb20661eff3b
Erin Shields <elshields92@gmail.com>
2020-11-24 14:51:06 -0300

    debug to false and add allowed hosts

--- server/server/settings.py
+++ server/server/settings.py
@@ -32,9 +32,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'http://viajera.herokuapp.com',
+    'localhost',
+    '127.0.0.1']
 
 
 # Application definition - add any apps you create with `startapp` here:
