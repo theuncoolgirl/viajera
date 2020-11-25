0d047fda47f03fd8bde626ea22fb54496d42dfc8
Erin Shields <elshields92@gmail.com>
2020-11-24 23:43:09 -0300

    update baseURL for axios

--- server/frontend/src/axiosApi.js
+++ server/frontend/src/axiosApi.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
-const baseURL = 'http://127.0.0.1:8000/api/'
+if (process.env.NODE_ENV === 'development') {
+    const baseURL = 'http://127.0.0.1:8000/api/'
+} else {
+    const baseURL = 'http://viajera.herokuapp.com/api/'
+}
+
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
