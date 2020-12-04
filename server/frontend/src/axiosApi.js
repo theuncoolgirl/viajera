a117023a746dddbcc3604827a7fe199ed436c22b
Erin Shields <elshields92@gmail.com>
2020-12-04 14:53:36 -0300

    update baseURL and env config

--- server/frontend/src/axiosApi.js
+++ server/frontend/src/axiosApi.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const baseURL = 'http://127.0.0.1:8000/api/'
 
 const axiosInstance = axios.create({
-    baseURL: baseURL,
+    baseURL: process.env.REACT_APP_BASE_URL,
     timeout: 5000,
     headers: {
         'Authorization': localStorage.getItem('access_token') ? "JWT " + localStorage.getItem('access_token') : null,
